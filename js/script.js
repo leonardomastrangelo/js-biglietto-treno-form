49bd64953140fc5ce8fba3491df47e010870b922
leonardomastrangelo <leo.mastrangelo.st@gmail.com>
2023-10-10 17:58:28 +0200

    styled html

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ const priceKm = 0.21;
 const discountMinor = 0.2;
 const discountSenior = 0.4;
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
 // ticket values
 submit.addEventListener("click",
@@ -29,11 +29,11 @@ submit.addEventListener("click",
             price.toFixed(2)
         }
 
-         // TICKET PRINT
-         let ticket = document.getElementById("ticket")
-         ticket.classList.remove("d-none")
-         // name
-         document.getElementById("name").innerHTML = lastName;
+        // TICKET PRINT
+        let ticket = document.getElementById("ticket")
+        ticket.classList.remove("d-none")
+        // name
+        document.getElementById("name").innerHTML = lastName;
         //  offer
         let type = document.getElementById("ticket-type")
         if (age === "minor") {
@@ -44,22 +44,22 @@ submit.addEventListener("click",
             type.innerHTML = "Standard Price"
         }
         // randomizer1
-        document.querySelector(".rndm1").innerHTML = getRndInteger(1,47);
-        // randomizer1
-        document.querySelector(".rndm2").innerHTML = getRndInteger(1,9999);
+        document.querySelector(".rndm1").innerHTML = getRndInteger(1, 47);
+        // randomizer2
+        document.querySelector(".rndm2").innerHTML = getRndInteger(1, 9999);
         // final price
-        document.getElementById("final-price").innerHTML = 
-        `
+        document.getElementById("final-price").innerHTML =
+            `
         &euro; ${price}
-        `; 
+        `;
     }
 )
 
-reset.addEventListener("click", 
+// ticket dismiss
+reset.addEventListener("click",
     function () {
-        // ticket dismiss
         let ticket = document.getElementById("ticket")
         ticket = ticket.classList.add("d-none")
-}
+    }
 )
 
